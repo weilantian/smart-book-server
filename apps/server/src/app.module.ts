770e24517d02bb57c7027c8806fb4a2a8b97b0bd
lantianwei <xiaolanlub@outlook.com>
2023-02-22 11:09:05 +1100

    feat: add database

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
